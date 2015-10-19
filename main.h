30232f7ab46b304c3be6736e927380e5e0ee5a1b
tuankhac <vutuankhac@gmail.com>
2015-10-19 19:54:26 +0700

    change

---
+++ main.h
@@ -0,0 +1,33 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx.h"
+#include "stm32f4xx_usart.h"
+#include "stm32f4_discovery_lis302dl.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
+#include "misc.h"
+#include <stdio.h>
+
+/* Exported types ------------------------------------------------------------*/
+
+/* Exported constants --------------------------------------------------------*/
+#define BT_BAUD 28800
+#define MAX_STRLEN 1 // this is the maximum string length of our string in characters
+
+/* Exported macro ------------------------------------------------------------*/
+
+/* Exported functions ------------------------------------------------------- */
+void TimingDelay_Decrement(void);
+void Delay(__IO uint32_t nTime);
+void init_LIS302DL();
+void init_USART1(uint32_t baudrate);
+//void initPA15();
+//void setPA15On();
+//void togglePA15();
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
